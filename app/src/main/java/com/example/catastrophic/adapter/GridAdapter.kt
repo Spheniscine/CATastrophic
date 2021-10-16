12df30873c62ecf3be580f472386ea67babeabf3
Spheniscine <spheniscine@gmail.com>
2021-10-16 15:49:06 +0800

    finally, recyclerview has something to show now

--- app/src/main/java/com/example/catastrophic/adapter/GridAdapter.kt
+++ app/src/main/java/com/example/catastrophic/adapter/GridAdapter.kt
@@ -1,24 +1,50 @@
 package com.example.catastrophic.adapter
 
+import android.graphics.drawable.Drawable
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
+import com.example.catastrophic.R
 
-class GridAdapter: RecyclerView.Adapter<GridAdapter.ImageViewHolder>() {
+class GridAdapter(fragment: Fragment, val drawables: List<Drawable>): RecyclerView.Adapter<GridAdapter.ImageViewHolder>() {
 
-    class ImageViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    val requestManager = Glide.with(fragment)
 
+
+    inner class ImageViewHolder(itemView: View):
+        RecyclerView.ViewHolder(itemView) {
+
+        val imageView = itemView.findViewById<ImageView>(R.id.item_image) ?:
+            error("ImageViewHolder requires inner ImageView with item_image id")
+        // TODO: on click listener
+
+        fun onBind() {
+            setImage()
+        }
+
+        fun setImage() {
+            requestManager
+                .load(drawables[adapterPosition])
+                .into(imageView)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_image, parent, false)
+        return ImageViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.onBind()
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return drawables.size
     }
 }
--- app/src/main/java/com/example/catastrophic/ui/main/GridFragment.kt
+++ app/src/main/java/com/example/catastrophic/ui/main/GridFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
 import com.example.catastrophic.R
+import com.example.catastrophic.adapter.GridAdapter
+import com.example.catastrophic.databinding.FragmentGridBinding
 
 class GridFragment : Fragment() {
 
@@ -14,19 +17,34 @@ class GridFragment : Fragment() {
         fun newInstance() = GridFragment()
     }
 
+    private var _binding: FragmentGridBinding? = null
+    // This property is only valid between onCreateView and
+// onDestroyView.
+    private val binding get() = _binding!!
+
     private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_grid, container, false)
+        _binding = FragmentGridBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.recyclerView.adapter = GridAdapter(this, List(18) {
+            AppCompatResources.getDrawable(requireContext(), R.drawable.cat_api_example_0)!!
+        })
+
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }

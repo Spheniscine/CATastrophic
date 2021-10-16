e1fb99f58522fc5c3fbd3c26fb82176556401ce9
Spheniscine <spheniscine@gmail.com>
2021-10-16 15:01:17 +0800

    Add adapter skeleton and Glide

---
+++ app/src/main/java/com/example/catastrophic/adapter/GridAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.catastrophic.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class GridAdapter: RecyclerView.Adapter<GridAdapter.ImageViewHolder>() {
+
+    class ImageViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}

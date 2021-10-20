8ca9a56e8598e69dea656148617d1aaccf6b27fb
Spheniscine <spheniscine@gmail.com>
2021-10-20 11:58:04 +0800

    paging test

--- app/src/test/java/com/example/catastrophic/repository/CatRepositoryTest.kt
+++ app/src/test/java/com/example/catastrophic/repository/CatRepositoryTest.kt
@@ -11,10 +11,12 @@ import retrofit2.Response
 
 class CatRepositoryTest {
 
+    val pageSize = CatRepository.PAGE_SIZE
+
     @Test
     fun `getCatData returns data from CatApiService`() {
         val catData = CatData(url = "some_url")
-        val response = Response.success(listOf(catData))
+        val response = Response.success(List(pageSize) { catData })
         val apiService: CatApiService = mockk()
         coEvery { apiService.getCats(any(), any()) } returns response
         val repository = CatRepository(apiService)
@@ -24,4 +26,27 @@ class CatRepositoryTest {
             assertEquals(catData, data)
         }
     }
+
+    fun testPaging(position: Int, pageNum: Int, indexInPage: Int) {
+        val list = List(pageSize) { i -> CatData(url = "url_$i") }
+        val response = Response.success(list)
+        val apiService: CatApiService = mockk()
+        coEvery { apiService.getCats(CatRepository.PAGE_SIZE, pageNum) } returns response
+        val repository = CatRepository(apiService)
+
+        runBlocking {
+            val data = repository.getCatData(position)
+            assertEquals(list[indexInPage], data)
+        }
+    }
+
+    @Test
+    fun `getCatData pages correctly`() {
+        testPaging(0, 1, 0)
+        testPaging(19, 1, 19)
+        testPaging(20, 2, 0)
+        testPaging(317, 16, 17)
+    }
+
+    
 }

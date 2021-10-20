80436030a0357df241f4c0ac460ae30c428d9081
Spheniscine <spheniscine@gmail.com>
2021-10-20 15:57:17 +0800

    re-fixed test

--- app/src/main/java/com/example/catastrophic/ui/fragment/GridFragment.kt
+++ app/src/main/java/com/example/catastrophic/ui/fragment/GridFragment.kt
@@ -48,7 +48,7 @@ class GridFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //scrollToPosition()
+        scrollToPosition()
     }
 
     override fun onDestroyView() {

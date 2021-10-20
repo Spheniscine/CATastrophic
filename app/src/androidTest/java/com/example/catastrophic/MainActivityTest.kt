e12e609f71f5b823d7c12f01ef87777a2b89f16d
Spheniscine <spheniscine@gmail.com>
2021-10-20 15:56:56 +0800

    broken test for scrolling viewpager affects recyclerview

--- app/src/androidTest/java/com/example/catastrophic/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/catastrophic/MainActivityTest.kt
@@ -1,15 +1,23 @@
 package com.example.catastrophic
 
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.swipeLeft
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.contrib.ViewPagerActions
+import androidx.test.espresso.matcher.BoundedMatcher
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.example.catastrophic.ui.adapter.GridAdapter
+import org.hamcrest.Description
+import org.hamcrest.Matcher
 import org.hamcrest.Matchers.not
 import org.junit.Rule
 import org.junit.Test
@@ -18,7 +26,7 @@ class MainActivityTest {
 
     @Rule
     @JvmField
-    var mActivityTestRule = ActivityScenarioRule<MainActivity>(MainActivity::class.java)
+    var activityScenarioRule = ActivityScenarioRule<MainActivity>(MainActivity::class.java)
 
     @Test
     fun recyclerView_isDisplayed() {
@@ -32,7 +40,7 @@ class MainActivityTest {
             .perform(RecyclerViewActions.actionOnItemAtPosition<GridAdapter.ImageViewHolder>(0, click()))
 
         Espresso.onView(withId(R.id.view_pager))
-            .check(ViewAssertions.matches(isDisplayed()))
+            .check(matches(isDisplayed()))
     }
 
     @Test
@@ -44,5 +52,33 @@ class MainActivityTest {
 
         Espresso.onView(withId(R.id.view_pager))
             .check(doesNotExist())
+    }
+
+    fun withViewAtPosition(position: Int, itemMatcher: Matcher<View>) =
+        object: BoundedMatcher<View, RecyclerView>(RecyclerView::class.java) {
+            override fun describeTo(description: Description?) {
+                itemMatcher.describeTo(description)
+            }
+
+            override fun matchesSafely(item: RecyclerView?): Boolean {
+                val vh = item?.findViewHolderForAdapterPosition(position)
+                return vh != null && itemMatcher.matches(vh.itemView)
+            }
+        }
+
+    @Test
+    fun scrolling_viewPager_affects_recyclerView_after_back() {
+        Espresso.onView(withId(R.id.recycler_view))
+            .perform(RecyclerViewActions.actionOnItemAtPosition<GridAdapter.ImageViewHolder>(0, click()))
+
+        repeat(25) {
+            Espresso.onView(withId(R.id.view_pager))
+                .perform(swipeLeft())
+        }
+
+        Espresso.pressBack()
+
+        Espresso.onView(withId(R.id.recycler_view))
+            .check(matches(withViewAtPosition(25, isDisplayed())))
     }
 }

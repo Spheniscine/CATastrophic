09a4bb95c88f2e7913c1a9be615299c7857a6f83
Spheniscine <spheniscine@gmail.com>
2021-10-20 15:26:21 +0800

    some comments

--- app/src/main/java/com/example/catastrophic/MainViewModel.kt
+++ app/src/main/java/com/example/catastrophic/MainViewModel.kt
@@ -15,6 +15,9 @@ import kotlinx.coroutines.launch
 
 class MainViewModel(private val catRepository: CatRepository) : ViewModel(), CatProvider by catRepository {
 
+    /** stores image position that view pager is looking at, used to scroll to correct position when going back to grid */
     var currentPosition = 0
+
+    /** whether grid recycler view should scroll */
     var shouldScroll = false
 }

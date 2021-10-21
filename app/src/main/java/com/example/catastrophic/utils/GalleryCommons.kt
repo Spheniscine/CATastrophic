5fb93f7972e2029b960070e6d82887c1e1e40035
Spheniscine <spheniscine@gmail.com>
2021-10-21 15:25:09 +0800

    more docs

--- app/src/main/java/com/example/catastrophic/utils/GalleryCommons.kt
+++ app/src/main/java/com/example/catastrophic/utils/GalleryCommons.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import android.graphics.drawable.Drawable
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 
+/** generates transition id string, keyed by position */
 fun transitionId(position: Int) = "image_transition_$position"
 
+/** generates animation when loading an image */
 fun loadingDrawable(context: Context): Drawable {
     return CircularProgressDrawable(context).apply {
         strokeWidth = 5f
